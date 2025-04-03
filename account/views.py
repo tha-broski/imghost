44a5376ede3a8485b1de6094ae6ad12e82fc0497
tha-broski <dabrowskimarcin2001@gmail.com>
2025-04-04 00:55:09 +0200

    Login added

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
-from account.forms import RegistrationForm
+from django.contrib.auth import login, authenticate, logout
+from account.forms import RegistrationForm, AccountAuthenticationForm
 
 # Create your views here.
 
@@ -19,7 +19,7 @@ def register_view(request, *args, **kwargs):
             raw_password = form.cleaned_data.get('password1')
             account = authenticate(email=email, password=raw_password)
             login(request, account)
-            destination = kwargs.get('next')
+            destination = get_redirect_if_exists(request)
             if destination:
                 return redirect(destination)
             return redirect('home')
@@ -27,3 +27,39 @@ def register_view(request, *args, **kwargs):
             context['registration_form'] = form
     
     return render(request, 'account/register.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
+
+def login_view(request, *args, **kwargs):
+
+    context = {}
+
+    user = request.user
+    if user.is_authenticated:
+        return redirect("home")
+    
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email').strip().lower()
+            password = form.cleaned_data.get('password')
+            user = authenticate(email = email, password = password)
+            if user:
+                login(request, user)
+                destination = get_redirect_if_exists(request)
+                if destination:
+                    return redirect(destination)
+                return redirect("home")
+        else:
+            context['login_form'] = form
+
+    return render(request, "account/login.html", context)
+
+def get_redirect_if_exists(request):
+    redirect = None
+    if request.GET:
+        if request.GET.get("next"):
+            redirect = str(request.GET.get("next"))
+    return redirect
--- imghost/urls.py
+++ imghost/urls.py
@@ -19,7 +19,11 @@ from django.contrib import admin
 from django.urls import path
 from imghostapp.views import home_view
 
-from account.views import register_view
+from account.views import (
+    register_view,
+    login_view,
+    logout_view,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +31,6 @@ urlpatterns = [
     path('', home_view, name='home'),
 
     path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
